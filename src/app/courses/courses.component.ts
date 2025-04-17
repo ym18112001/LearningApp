12f54d88ae25b379563d61609d05ff9538f64d5e
Mohamed20384 <Mohamed20384@fci.bu.edu.eg>
2025-04-17 23:29:52 +0200

    complete the course page

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -98,7 +98,6 @@ export class CoursesComponent {
     ];
 
     selectedCategory = this.categories[0];
-    // In your component.ts file
     getStarRating(rating: number): { stars: string, display: string } {
       const fullStars = Math.floor(rating);
       const hasHalfStar = rating % 1 >= 0.5;
@@ -111,6 +110,41 @@ export class CoursesComponent {
       const display = `${rating} ${stars}`;
       
       return { stars, display };
-}
+    }
 
-}
+    selectedImage = 'https://cms-images.udemycdn.com/96883mtakkm8/4kbyXne3Slx9Sfz4nTBqdf/8ac2b75db1a118f15e2fb5dfe2bb4567/desktop-hands-on-learning-2x.png';
+
+    goals = [
+      {
+        title: 'Hands-on training',
+        desc: 'Upskill effectively with AI-powered coding exercises, practice tests, and quizzes.',
+        image: 'https://cms-images.udemycdn.com/96883mtakkm8/4kbyXne3Slx9Sfz4nTBqdf/8ac2b75db1a118f15e2fb5dfe2bb4567/desktop-hands-on-learning-2x.png',
+        icon: 'https://cms-images.udemycdn.com/96883mtakkm8/7kN9RBFSMFNHzsGWsElMPi/dde73f8d1c47e046f035274e78410590/hands-on-practice.png',
+      },
+      {
+        title: 'Certification prep',
+        desc: 'Prep for industry-recognized certifications and earn badges along the way.',
+        image: 'https://cms-images.udemycdn.com/96883mtakkm8/GUVYFTj0uwEQuJha5j7TZ/473be949e2751dd5826b141dc4c16892/desktop-certification-prep-2x.png',
+        icon: 'https://cms-images.udemycdn.com/96883mtakkm8/2Xh9YHJustDwCEjn5IlO25/93e9b15c6e74876db0dec63466fcc5a0/certificate.png',
+      },
+      {
+        title: 'Insights and analytics',
+        desc: 'Advanced insights and a dedicated customer success team.',
+        image: 'https://cms-images.udemycdn.com/96883mtakkm8/6q4N9BvIQusFoheoALJhGj/678c1a0bb6c2a22d95461d409492231e/desktop-insights-and-analytics-2x.png',
+        icon: 'https://cms-images.udemycdn.com/96883mtakkm8/6w8plrr7vY9rIY46UuX0q5/2f0a3f0c22e99bd2d430b998c81321f2/empty-state-1.png',
+        badge: 'Enterprise only'
+      },
+      {
+        title: 'Customizable content',
+        desc: 'Create tailored learning paths and host your own content.',
+        image: 'https://cms-images.udemycdn.com/96883mtakkm8/385IhnON960Wvz50ooWIN3/d4e6738c97769258d387b3d609edaad4/desktop-customizable-2x.png',
+        icon: 'https://cms-images.udemycdn.com/96883mtakkm8/2tKGBrb1N60wox2Lh8j3tz/7f1528c9f88ea47bd6ebb46f345902c3/organizations-2.png',
+        badge: 'Enterprise plan'
+      }
+    ];
+  
+    setActive(imagePath: string): void {
+      this.selectedImage = imagePath;
+    }
+    
+  }
